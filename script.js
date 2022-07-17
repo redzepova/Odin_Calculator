145986c425cdec0eabbb0c4a26b45c7cbe05e545
Kendra Moon <kendra.moon@gmail.com>
2022-07-17 11:45:54 -0500

    add function to create input number as string for 2 inputs

--- script.js
+++ script.js
@@ -2,27 +2,38 @@ const addition = ((a, b) => a + b);
 const subtraction = ((a, b) => a - b);
 const multiply = ((a, b) => a * b);
 const division = ((a, b) => a/b);
-let operatorSelected = false;
+let operatorSelected = true;
 let firstNumber = '';
 let secondNumber = '';
 let result = 0;
 const numberButtons = document.querySelectorAll('.number-button');
 const operatorButtons = document.querySelectorAll('.operator-button');
 
+//this function adds an event listener to any node list passed to it//
 function addEvents (nodeList) {
     for (let i = 0; i < nodeList.length; i++) {
         nodeList[i].addEventListener('click', () => {
-            console.log(nodeList[i].textContent);
+            let tempNumber = nodeList[i].textContent;
+            !operatorSelected ? firstNumber = createInput(firstNumber, tempNumber) : secondNumber = createInput(secondNumber, tempNumber);
+            console.log(firstNumber);
+            console.log(secondNumber);
         });
+
     };
 };
 
+//add event listeners to various buttons via nodelist//
 addEvents(numberButtons);
 
 addEvents(operatorButtons);
 
+//create a concatenated string each time number button is pressed//
+const createInput = ((a,b) => a = a + b);
 
+//convert the string of 'numbers' into an actual number//
+const getNumber = (a => parseFloat(a));
 
+//operator functions//
 const operate = function(operator, a, b) {
     if (operator === '+') {
         addition(a, b);
