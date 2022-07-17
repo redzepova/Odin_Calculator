a351c5e871d9a5d647d2d7665117222316d10405
Kendra Moon <kendra.moon@gmail.com>
2022-07-17 11:18:56 -0500

    create event listener function

--- script.js
+++ script.js
@@ -3,26 +3,35 @@ const subtraction = ((a, b) => a - b);
 const multiply = ((a, b) => a * b);
 const division = ((a, b) => a/b);
 let operatorSelected = false;
-const numberButton = document.querySelectorAll('.number-button');
+let firstNumber = '';
+let secondNumber = '';
+let result = 0;
+const numberButtons = document.querySelectorAll('.number-button');
+const operatorButtons = document.querySelectorAll('.operator-button');
 
-console.log(numberButton);
-
-for(let i = 0; i < numberButton.length; i++) {
-    numberButton[i].addEventListener('click', () => {
-        console.log(numberButton[i].textContent);
-    });
+function addEvents (nodeList) {
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].addEventListener('click', () => {
+            console.log(nodeList[i].textContent);
+        });
+    };
 };
+
+addEvents(numberButtons);
+
+addEvents(operatorButtons);
+
 
 
 const operate = function(operator, a, b) {
-    if (operator === 'add') {
-        addition(a, num2);
-    } else if (operator === 'minus') {
-        subtraction(num1, num2);
-    } else if (operator === 'times') {
-        multiply(num1, num2);
-    } else if (operator === 'divide') {
-        division(num1, num2);
+    if (operator === '+') {
+        addition(a, b);
+    } else if (operator === '-') {
+        subtraction(a, b);
+    } else if (operator === 'x') {
+        multiply(a, b);
+    } else if (operator === '%') {
+        division(a, b);
     } else {
         return 'error';
     };
